50cf1ecf11fcc98f03b8a9d9aebe8ed0c6267f1e
engboris <engboris@hotmail.fr>
2024-08-11 19:26:52 +0200

    Add testsuite

---
+++ test/test.ml
@@ -0,0 +1,29 @@
+open Alcotest
+open Base
+open Lsclib.Stellar
+open Lsclib.Parser
+
+let test filename expected () =
+  let lexbuf = Lexing.from_channel (Stdlib.open_in filename) in
+  let mcs = marked_constellation Lsclib.Lexer.read lexbuf in
+  let cs = extract_intspace mcs in
+  let result =
+    exec ~unfincomp:false
+         ~withloops:false
+         ~showtrace:false
+         ~selfint:false
+         ~showsteps:false cs
+         |> string_of_constellation in
+  check string "same string" result expected
+
+let example filename = "../examples/" ^ filename
+
+let suite =
+  [ "Automata", `Quick, test (example "automata/nfsa_ending00.stellar") "accept;"
+  ; "Prolog", `Quick, test (example "logicprogramming/prolog.stellar") "s(s(s(s(0))));"
+  ; "MLL (cut-elim)", `Quick, test (example "mll/cut.stellar") "6(X33) 3(X33);"
+  ; "MLL (correctness)", `Quick, test (example "mll/correctness.stellar") "c5(X53) c6(X53);"
+  ]
+
+let () =
+  Alcotest.run "Stellar Resolution" [ "Basic tests", suite ]
