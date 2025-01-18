a6c73f0721dfe8496e0ccacbbf18bd97a0d5450f
zapashcanon <leo@ndrs.fr>
2025-01-18 12:27:48 +0100

    fmt

--- bin/sgen.ml
+++ bin/sgen.ml
@@ -14,19 +14,19 @@ let speclist = []
 
 let print_position outx lexbuf =
   let pos = lexbuf.lex_curr_p in
-  Stdlib.Printf.fprintf outx "%s:%d:%d" pos.pos_fname
-    pos.pos_lnum (pos.pos_cnum - pos.pos_bol)
+  Stdlib.Printf.fprintf outx "%s:%d:%d" pos.pos_fname pos.pos_lnum
+    (pos.pos_cnum - pos.pos_bol)
 
 let parse_with_error lexbuf =
-  try program read lexbuf with
-  | SyntaxError msg ->
+  try program read lexbuf
+  with SyntaxError msg ->
     Stdlib.Printf.fprintf Stdlib.stderr "%a: %s\n" print_position lexbuf msg;
     Stdlib.exit (-1)
 
 let () =
   Stdlib.Arg.parse speclist anon_fun usage_msg;
   let lexbuf = Lexing.from_channel (Stdlib.open_in !input_file) in
-  lexbuf.lex_curr_p <- {lexbuf.lex_curr_p with pos_fname = !input_file};
+  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = !input_file };
   let p = parse_with_error lexbuf in
   let _ = eval_program p in
   ()
